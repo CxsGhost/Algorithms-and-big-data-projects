0ced502d40e81a82cd32be537632138ecceeb853
CxsGhost <1659101919@qq.com>
2019-12-13 12:50:08 +0800

    快排

--- 1.py
+++
@@ -1,7 +0,0 @@
-import random
-import time
-people = ['kingname', '王小一', '李小二', '张小三', '刘小四', '卢小五', '马小六', '周小七', '丁小八', '朱小九']
-for i in range(1, 11):
-    lucky_guy = random.choice(people)
-    print(f'第{i}次抽奖，中奖用户：{lucky_guy}')
-    time.sleep(1)

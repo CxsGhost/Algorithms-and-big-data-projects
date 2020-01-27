ad774d881ef36cf2da3eb984fdb8a20eaf10aee9
CxsGhost <1659101919@qq.com>
2020-01-27 17:30:21 +0800

    快排

--- 感知机/感知机.py
+++ 感知机/感知机.py
@@ -2,13 +2,16 @@ import numpy as np
 import copy
 import matplotlib.pyplot as plt
 
+# 创建numpy数组必须声明浮点数类型，否则当学习率不为1时，会导致参数更新自动取整，进而会出现迭代无法完成的情况
+# 包括收录数据时，也要声明为浮点类型，否则就导致数据自动变为取整型int32
+
 
 class Perceptron:
     def __init__(self):
-        self.learn_rate = 1
+        self.learn_rate = 0.5
         self.w_b = np.array([[0],
                             [0],
-                            [0]])  # 把三个参数放在一个矩阵中（w1, w2, b)
+                            [0]], dtype='float32')  # 把三个参数放在一个矩阵中（w1, w2, b）
         self.t_data = None
         self.t_data_c = None  # 因为要三个参数一起更新，观察书上的公式，其实就是y乘x1,x2,1,然后加到参数矩阵就行了所以又建立一个y值全是1的矩阵
 
@@ -17,7 +20,7 @@ class Perceptron:
         collect_2 = []
         while True:
             try:  # 利用异常处理结束输入，别的没想到啥好办法。。
-                data = map(int, input("输入数据x1 x2 y（空格隔开)\
+                data = map(float, input("输入数据x1 x2 y（空格隔开)\
                 输入任意字母敲回车以结束：").split(' '))
                 data = list(data)
                 collect_1.append(copy.copy(data))  # 测试发现，下一行的更改会影响上一行，两个指向的变量地址一样，所以得copy一下
@@ -26,23 +29,30 @@ class Perceptron:
             except ValueError:
                 print("数据收集完毕！")
                 break
-        self.t_data = np.array(collect_1)
-        self.t_data_c = np.array(collect_2)
+        self.t_data = np.array(collect_1, dtype='float32')
+        self.t_data_c = np.array(collect_2, dtype='float32')
 
     def gradient_descent(self):
         print("开始迭代...")
-        while True:  # 每次都统计误分类的点数，直到没有为止，跳出循环，迭代结束
+        number = 0
+        while True:
+            number += 1
+            # 每次都统计误分类的点数，直到没有为止，跳出循环，迭代结束
             mistake = 0
             for line, line_c in zip(self.t_data, self.t_data_c):  # 一个用来判断是否误分类，一个用来更新参数矩阵
                 if line[2] * np.dot(line_c, self.w_b)[0] <= 0:
                     line_c = line_c * line[2] * self.learn_rate  # 更新方法对应着上面第二条注释
+                    print(line_c)
                     mistake += 1
                     self.w_b[0][0] += line_c[0]
                     self.w_b[1][0] += line_c[1]
                     self.w_b[2][0] += line_c[2]
             if mistake == 0:
                 break
+            print('第{}次迭代\n参数w：{}'.format(number, self.w_b))
+        print('-----------------')
         print("迭代完成！")
+        print('学习率：{}，迭代次数：{}'.format(self.learn_rate, number))
 
     def visualize(self):  # 以下绘图中的y并不是之前的那个y,其实是所谓的x_2。。
         plt.figure(figsize=(8, 4))
